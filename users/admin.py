5196b80353662bef6f39ac249989fe37705e9e28
KhaiCoding <KhaiCoding@github.com>
2019-10-16 18:50:11 +0900

    user admin fields
    making user model admin pannel

--- users/admin.py
+++ users/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from . import models
 
 # Register your models here.
+@admin.register(models.User)
+class CustomUserAdmin(admin.ModelAdmin):
+
+    """ Custom User Admin """
+
+    # admin/users/user field value
+    list_display = ("username", "email", "gender", "language", "currency", "superhost")
+    list_filter = ("gender", "language", "currency", "superhost")
